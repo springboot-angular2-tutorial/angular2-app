9cac368db0a4acfaff61b0f95c931dbba2a69a9f
Akira Sosa <akirasosa@gmail.com>
2016-07-05 16:01:04 +0700

    decorateComponentRef

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
 import {ComponentRef} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
-import {FORM_PROVIDERS} from "@angular/common";
-import {HTTP_PROVIDERS} from "@angular/http";
-import {ENV_PROVIDERS} from "./platform/environment";
+import {ENV_PROVIDERS, decorateComponentRef} from "./platform/environment";
 import {AppComponent, APP_PROVIDERS, appInjector} from "./app";
 import {PLATFORM_PROVIDERS} from "./platform/browser";
 
 function main():Promise<any> {
   return bootstrap(AppComponent, [
     ...PLATFORM_PROVIDERS,
-    // ...FORM_PROVIDERS,
     ...ROUTER_PROVIDERS,
-    // ...HTTP_PROVIDERS,
     ...ENV_PROVIDERS,
     ...APP_PROVIDERS,
   ]).then((appRef:ComponentRef<any>) => {
     appInjector(appRef.injector);
+    decorateComponentRef(appRef);
   }).catch(e => console.error(e));
 }
 
