8c5b68e65baa940ca68b7b2d3da7530a403ce1bf
Akira Sosa <akirasosa@gmail.com>
2016-07-11 11:25:26 +0700

    fix spec for http-error-handler

--- src/shared/services/http-error-handler.spec.ts
+++ src/shared/services/http-error-handler.spec.ts
@@ -1,9 +1,10 @@
-import {Router} from "@angular/router-deprecated";
-import {inject, beforeEachProviders, beforeEach} from "@angular/core/testing";
+import {inject, addProviders} from "@angular/core/testing";
+import {Router} from "@angular/router";
 import {HttpErrorHandler} from "./http-error-handler";
 import {LoginService} from "./login.service";
-import {APP_TEST_PROVIDERS} from "../../app";
-import {prepareAppInjector} from "../testing/helpers";
+import {APP_TEST_HTTP_PROVIDERS} from "../http/index";
+import {APP_SERVICE_PROVIDERS} from "./index";
+import {MockRouter} from "../testing/mock-router";
 
 describe('HttpErrorHandler', () => {
 
@@ -11,8 +12,14 @@ describe('HttpErrorHandler', () => {
   let loginService:LoginService;
   let router:Router;
 
-  beforeEachProviders(() => [APP_TEST_PROVIDERS]);
-  beforeEach(prepareAppInjector());
+  beforeEach(() => addProviders([
+    {
+      provide: Router,
+      useClass: MockRouter,
+    },
+    ...APP_TEST_HTTP_PROVIDERS,
+    ...APP_SERVICE_PROVIDERS,
+  ]));
   beforeEach(inject([HttpErrorHandler, LoginService, Router], (..._) => {
     [errorHandler, loginService, router] = _;
     spyOn(loginService, 'logout');
@@ -23,7 +30,7 @@ describe('HttpErrorHandler', () => {
     it('handles 401 response', () => {
       errorHandler.handle({status: 401});
       expect(loginService.logout).toHaveBeenCalled();
-      expect(router.navigate).toHaveBeenCalledWith(['/Login']);
+      expect(router.navigate).toHaveBeenCalledWith(['login']);
     });
 
     it('does not handle other errors', () => {
--- src/shared/testing/mock-router.ts
+++ src/shared/testing/mock-router.ts
@@ -1,2 +1,6 @@
 export class MockRouter {
+
+  navigate() {
+  }
+
 }
