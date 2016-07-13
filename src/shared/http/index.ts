f606c8391d783ecaea339deb8d7d8651bc6b5a98
Akira Sosa <akirasosa@gmail.com>
2016-07-13 17:20:30 +0700

    replace old provide

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -1,4 +1,3 @@
-import {provide} from "@angular/core";
 import {Http, HTTP_PROVIDERS, XHRBackend, RequestOptions} from "@angular/http";
 import {MockBackend} from "@angular/http/testing";
 import {MyHttp} from "./http";
@@ -7,23 +6,25 @@ export * from './http';
 
 export const APP_HTTP_PROVIDERS = [
   HTTP_PROVIDERS,
-  provide(MyHttp, {
+  {
+    provide: MyHttp,
     useFactory: (xhrBackend:XHRBackend, requestOptions:RequestOptions) => {
       const ngHttp = new Http(xhrBackend, requestOptions);
       return new MyHttp(ngHttp);
     },
     deps: [XHRBackend, RequestOptions]
-  })
+  },
 ];
 
 export const APP_TEST_HTTP_PROVIDERS = [
   HTTP_PROVIDERS,
   MockBackend,
-  provide(MyHttp, {
+  {
+    provide: MyHttp,
     useFactory: (mockBackend:MockBackend, requestOptions:RequestOptions) => {
       const http = new Http(mockBackend, requestOptions);
       return new MyHttp(http);
     },
     deps: [MockBackend, RequestOptions]
-  })
+  },
 ];
--- src/shared/testing/helpers.ts
+++ src/shared/testing/helpers.ts
@@ -1,8 +1,5 @@
 import {inject, ComponentFixture, tick} from "@angular/core/testing";
-import {Router} from "@angular/router-deprecated";
-import {ReflectiveInjector, provide} from "@angular/core";
-import {appInjector} from "../../app";
-import {LoginService, UserService} from "../services";
+import {LoginService} from "../services";
 
 export function login() {
   return inject([LoginService], (loginService) => {
@@ -10,17 +7,7 @@ export function login() {
   });
 }
 
-export function prepareAppInjector():Function {
-  return inject([UserService, LoginService, Router], (userService, loginService, router) => {
-    appInjector(ReflectiveInjector.resolveAndCreate([
-      provide(UserService, {useValue: userService}),
-      provide(LoginService, {useValue: loginService}),
-      provide(Router, {useValue: router}),
-    ]));
-  });
-}
-
-export function advance(fixture: ComponentFixture<any>): void {
+export function advance(fixture:ComponentFixture<any>):void {
   tick();
   fixture.detectChanges();
 }
