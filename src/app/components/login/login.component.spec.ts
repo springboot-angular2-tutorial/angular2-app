5bca5c8f8d43f3c8969731538596f03d4ccb7aa1
Akira Sosa <akirasosa@gmail.com>
2016-07-13 14:39:28 +0700

    fix login component spec

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,48 +1,48 @@
 import {Component, DebugElement} from "@angular/core";
 import {By} from "@angular/platform-browser/src/dom/debug/by";
 import {getDOM} from "@angular/platform-browser/src/dom/dom_adapter";
-import {inject, async, addProviders} from "@angular/core/testing";
+import {inject, async, addProviders, fakeAsync} from "@angular/core/testing";
 import {BaseResponseOptions, Response} from "@angular/http";
 import {Location} from "@angular/common";
-import {LoginComponent} from "./login.component";
-import {LoginService, APP_SERVICE_PROVIDERS} from "../../../shared/services";
 import {
   TestComponentBuilder,
   ComponentFixture
 } from "@angular/compiler/testing";
 import {MockBackend} from "@angular/http/testing";
-import {Router} from "@angular/router";
+import {Router, ROUTER_DIRECTIVES} from "@angular/router";
+import {LoginComponent} from "./login.component";
+import {LoginService, APP_SERVICE_PROVIDERS} from "../../../shared/services";
 import {provideFakeRouter} from "../../../shared/routes/router-testing-providers";
 import {APP_TEST_HTTP_PROVIDERS} from "../../../shared/http/index";
-import {ROUTER_DIRECTIVES} from "@angular/router-deprecated";
+import {advance} from "../../../shared/testing/helpers";
 
 describe('LoginComponent', () => {
 
   @Component({
-    selector: 'blank-cmp',
-    template: ``,
-    directives: ROUTER_DIRECTIVES
-  })
-  class BlankCmp {
-  }
-
-
-  @Component({
-    template: `<mpt-login></mpt-login>`,
-    directives: [LoginComponent],
+    template: `<mpt-login></mpt-login><router-outlet></router-outlet>`,
+    directives: [LoginComponent, ROUTER_DIRECTIVES],
   })
   class TestComponent {
   }
 
+  @Component({
+    template: ``,
+  })
+  class BlankComponent {
+  }
+
   let cmpDebugElement:DebugElement;
 
+  let fixture:ComponentFixture<any>;
   let loginService:LoginService;
   let backend:MockBackend;
   let router:Router;
   let location:Location;
 
   beforeEach(() => addProviders([
-    provideFakeRouter(TestComponent),
+    provideFakeRouter(TestComponent, [
+      {path: 'home', component: BlankComponent},
+    ]),
     ...APP_TEST_HTTP_PROVIDERS,
     ...APP_SERVICE_PROVIDERS,
   ]));
@@ -52,9 +52,10 @@ describe('LoginComponent', () => {
   beforeEach(async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
     tcb
       .createAsync(TestComponent)
-      .then((fixture:ComponentFixture<any>) => {
-        cmpDebugElement = fixture.debugElement.query(By.directive(LoginComponent));
-        fixture.detectChanges();
+      .then((_fixture:ComponentFixture<any>) => {
+        fixture = _fixture;
+        cmpDebugElement = _fixture.debugElement.query(By.directive(LoginComponent));
+        _fixture.detectChanges();
       });
   })));
 
@@ -62,7 +63,7 @@ describe('LoginComponent', () => {
     expect(cmpDebugElement).toBeTruthy();
   });
 
-  it('can login', (done) => {
+  it('can login', fakeAsync(() => {
     const cmp:LoginComponent = cmpDebugElement.componentInstance;
     spyOn(loginService, 'login').and.callThrough();
     backend.connections.subscribe(conn => {
@@ -70,16 +71,14 @@ describe('LoginComponent', () => {
     });
     cmp.login('test@test.com', 'secret');
     expect(loginService.login).toHaveBeenCalledWith('test@test.com', 'secret');
-    router.subscribe(() => {
-      expect(location.path()).toEqual('/home');
-      done();
-    });
-  });
+    advance(fixture);
+    expect(location.path()).toEqual('/home');
+  }));
 
   it('can navigate to signup page', () => {
     const el = cmpDebugElement.nativeElement;
-    getDOM().querySelector(el, 'a').click();
-    expect(location.path()).toEqual('/signup');
+    const signupLink = getDOM().querySelector(el, 'a');
+    expect(signupLink.getAttribute('href')).toEqual('/signup');
   });
 
 });
