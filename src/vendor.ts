14bcc4f8c9fe02deddfd8071fcc17622f63e2da6
Akira Sosa <akirasosa@gmail.com>
2016-07-14 10:57:51 +0700

    replace typings with @types

--- src/vendor.ts
+++ src/vendor.ts
@@ -10,7 +10,6 @@ import "rxjs/add/operator/map";
 import "rxjs/add/operator/mergeMap";
 import "rxjs/add/operator/finally";
 import "rxjs/add/operator/do";
-import "rxjs/add/operator/toPromise";
 
 require("expose?$!expose?jQuery!jquery");
 require('bootstrap-loader');
