8af7bc0e326a2084fc543d7bd1a0c00808efd33f
Akira Sosa <akirasosa@gmail.com>
2016-07-14 11:27:47 +0700

    introduce @types/pluralize

--- src/shared/pipes/pluralize.pipe.ts
+++ src/shared/pipes/pluralize.pipe.ts
@@ -1,6 +1,5 @@
 import {Pipe, PipeTransform} from "@angular/core";
-
-const pluralize = require('pluralize');
+import * as pluralize from "pluralize";
 
 @Pipe({name: 'pluralize'})
 export class PluralizePipe implements PipeTransform {
