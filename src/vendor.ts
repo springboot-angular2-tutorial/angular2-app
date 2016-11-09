56384abeab2dca97bcb9c49f41e2f06ef84fa256
Akira Sosa <akirasosa@gmail.com>
2016-11-09 21:30:22 +0700

    optimize rxjs impoting

--- src/vendor.ts
+++ src/vendor.ts
@@ -2,6 +2,8 @@ import "rxjs/add/operator/map";
 import "rxjs/add/operator/mergeMap";
 import "rxjs/add/operator/finally";
 import "rxjs/add/operator/do";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/delay";
 import "lodash/values";
 import "lodash/isEmpty";
 import "lodash/omitBy";
