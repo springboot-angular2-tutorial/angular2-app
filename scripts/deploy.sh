fbb20243f73468c4c237ce330b278b290fca5acd
Akira Sosa <akirasosa@gmail.com>
2016-12-11 08:58:31 +0700

    ensure docker repo exists

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,17 @@ if [ ! -v AWS_SESSION_TOKEN ]; then
   source ./scripts/switch-role.sh
 fi
 
-# build
-PUBLIC_PATH="https://cdn-${ENV}.hana053.com" yarn run build
-
-# update container
 readonly DOCKER_NAME=micropost/frontend
 readonly AWS_ACCOUNT_NUMBER=$(aws sts get-caller-identity --output text --query 'Account')
+
+# Build
+PUBLIC_PATH="https://cdn-${ENV}.hana053.com" yarn run build
+
+# Ensure docker repository exists
+aws ecr describe-repositories --repository-names ${DOCKER_NAME} || \
+  aws ecr create-repository --repository-name ${DOCKER_NAME}
+
+# Push to docker repository
 eval $(aws ecr get-login)
 docker build -t ${DOCKER_NAME} .
 docker tag ${DOCKER_NAME}:latest ${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_NAME}:latest
