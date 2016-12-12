80b0406145d4fd2847a57ca988b0b18b1fc0f566
Akira Sosa <akirasosa@gmail.com>
2016-12-12 15:08:04 +0700

    use ecs-deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ fi
 
 readonly DOCKER_NAME=micropost/frontend
 readonly AWS_ACCOUNT_NUMBER=$(aws sts get-caller-identity --output text --query 'Account')
+readonly IMAGE_URL=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_NAME}
 
 # Build
 PUBLIC_PATH="https://cdn-${ENV}.hana053.com" yarn run build
@@ -19,8 +20,8 @@ aws ecr describe-repositories --repository-names ${DOCKER_NAME} || \
 # Push to docker repository
 eval $(aws ecr get-login)
 docker build -t ${DOCKER_NAME} .
-docker tag ${DOCKER_NAME}:latest ${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_NAME}:latest
-docker push ${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_NAME}:latest
+docker tag ${DOCKER_NAME}:latest ${IMAGE_URL}:latest
+docker push ${IMAGE_URL}:latest
 
-# Update service
-aws ecs update-service --cluster micropost --service frontend
+# Deploy
+./scripts/ecs-deploy -c micropost -n frontend -i ${IMAGE_URL}:latest
