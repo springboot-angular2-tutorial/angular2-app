bad7b029456d65e5ffe364d17d46cfc073c38b18
Akira Sosa <akirasosa@gmail.com>
2016-12-12 15:34:47 +0700

    do not show useless information

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ readonly IMAGE_URL=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws
 PUBLIC_PATH="https://cdn-${ENV}.hana053.com" yarn run build
 
 # Ensure docker repository exists
-aws ecr describe-repositories --repository-names ${DOCKER_NAME} || \
+aws ecr describe-repositories --repository-names ${DOCKER_NAME} > /dev/null 2>&1 || \
   aws ecr create-repository --repository-name ${DOCKER_NAME}
 
 # Push to docker repository
